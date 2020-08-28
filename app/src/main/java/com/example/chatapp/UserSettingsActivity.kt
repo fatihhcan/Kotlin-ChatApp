62ca3031b180252290b84e27cbe7c4bad473fe3d
Fatih Can <ybsfatihcan@gmail.com>
2020-08-28 09:42:49 +0300

    Mail and password updates have been completed.

--- app/src/main/java/com/example/chatapp/UserSettingsActivity.kt
+++ app/src/main/java/com/example/chatapp/UserSettingsActivity.kt
@@ -1,8 +1,11 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_user_settings.*
@@ -14,7 +17,7 @@ class UserSettingsActivity : AppCompatActivity() {
         var user=FirebaseAuth.getInstance().currentUser!!
 
         editTextDetailName.setText(user.displayName.toString())
-        editTextDetailMail.setText(user.email.toString())
+
 
         btnPasswordSend.setOnClickListener {
             FirebaseAuth.getInstance().sendPasswordResetEmail(FirebaseAuth.getInstance().currentUser?.email.toString())
@@ -32,7 +35,7 @@ class UserSettingsActivity : AppCompatActivity() {
         }
 
         btnSaveChanges.setOnClickListener {
-            if (editTextDetailName.text.toString().isNotEmpty() && editTextDetailMail.text.toString().isNotEmpty())
+            if (editTextDetailName.text.toString().isNotEmpty())
             {
                 if (!editTextDetailName.text.toString().equals(user.displayName.toString()))
                     {
@@ -54,6 +57,72 @@ class UserSettingsActivity : AppCompatActivity() {
                 Toast.makeText(this,"Fill in  the blank fields.", Toast.LENGTH_SHORT).show()
             }
         }
+
+        btnPasswordMailUpdate.setOnClickListener {
+            if (editTextDetailPassword.text.toString().isNotEmpty())
+            {
+                var credential=EmailAuthProvider.getCredential(user.email.toString(),editTextDetailPassword.text.toString())
+                user.reauthenticate(credential)
+                    .addOnCompleteListener { task->
+                        if (task.isSuccessful){
+                                updateLayout.visibility= View.VISIBLE
+                            btnUpdateMail.setOnClickListener {
+                                mailAddressUpdate()
+                            }
+
+                            btnUpdatePassword.setOnClickListener {
+                                passwordUpdate()
+                            }
+                        }else{
+                            Toast.makeText(this,"You entered your current password incorrectly.", Toast.LENGTH_SHORT).show()
+                            updateLayout.visibility= View.INVISIBLE
+
+                        }
+                    }
+            }else{
+                Toast.makeText(this,"Enter your valid password for updates.", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    private fun passwordUpdate() {
+
+        var user=FirebaseAuth.getInstance().currentUser!!
+
+        if (user !=null){
+            user.updatePassword(editTextNewPassword.text.toString())
+                .addOnCompleteListener { task ->
+                    Toast.makeText(this,"Password has been changed.", Toast.LENGTH_SHORT).show()
+                    FirebaseAuth.getInstance().signOut()
+                    loginPageRoute()
+                }
+        }
+    }
+
+    private fun mailAddressUpdate() {
+        var user=FirebaseAuth.getInstance().currentUser!!
+
+        if (user !=null){
+
+
+
+
+            user.updateEmail(editTextNewMail.text.toString())
+                .addOnCompleteListener { task ->
+
+                    Toast.makeText(this,"Mail address has been changed.", Toast.LENGTH_SHORT).show()
+                    FirebaseAuth.getInstance().signOut()
+                    loginPageRoute()
+
+
+                }
+        }
+    }
+    fun loginPageRoute(){
+        var intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
 
